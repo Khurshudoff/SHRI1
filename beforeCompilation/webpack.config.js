ba37635021cb10e1b12331bfda6367394be43a48
Артем Хуршудов <khurshudov@MacBook-Pro-Artem.local>
2018-10-25 01:14:30 +0300

    qweqwe

--- beforeCompilation/webpack.config.js
+++ beforeCompilation/webpack.config.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        app: './src/index.js',
+        index: './src/index.js',
     },
     devtool: 'inline-source-map',
     devServer: {
